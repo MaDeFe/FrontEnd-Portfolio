ae176dd6cb10cf8284e6e40cb2285c58e79273e9
MaDeFe <marianapaulamolina@gmail.com>
2023-05-14 16:18:00 -0300

    Crear components

--- frontend-angular/src/app/@components/hero/hero.module.ts
+++ frontend-angular/src/app/@components/hero/hero.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroComponent } from './hero.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeroComponent
+  ],
   imports: [
     CommonModule
   ]
--- frontend-angular/src/app/@components/skills/skills.module.ts
+++ frontend-angular/src/app/@components/skills/skills.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SkillsComponent } from './skills.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SkillsComponent
+  ],
   imports: [
     CommonModule
   ]
